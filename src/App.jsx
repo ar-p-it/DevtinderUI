11c847710331cc406fb6a9e20b118c301375e6c9
Arpiit k <kesariarpit5@gmail.com>
2026-01-08 10:25:35 +0530

    daisy ui and tailwind combined

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
-
+// Tailwind is already imported via src/index.css
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Vite + React + Tailwind
+      </h1>
     </>
   );
 }

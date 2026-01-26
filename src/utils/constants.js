4504d4ff73fae4b79697b4c8439e823329aa3793
Arpiit k <kesariarpit5@gmail.com>
2026-01-26 20:37:08 +0530

    /api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 // In dev, Vite proxies /api to http://localhost:3050 (see vite.config.js)
-export const BASE_URL = "http://localhost:3050";
+// export const BASE_URL = "http://localhost:3050";
+export const BASE_URL = "/api";
